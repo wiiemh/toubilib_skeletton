069f9732ca193b7d2ed1275cec94c88ba35ec913
Hmem Wiem <wiem.hmem@outlook.com>
2025-09-09 15:45:22 +0200

    praticien.php complet

--- app/src/application_core/domain/entities/praticien/Praticien.php
+++ app/src/application_core/domain/entities/praticien/Praticien.php
@@ -11,4 +11,18 @@ class Praticien
    private string $email;
    private string $specialite;
 
+    public function __construct(string $nom, string $prenom, string $ville, string $email, string $specialite) {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->ville = $ville;
+        $this->email = $email;
+        $this->specialite = $specialite;
+    }
+
+    // Getters
+    public function getNom(): string { return $this->nom; }
+    public function getPrenom(): string { return $this->prenom; }
+    public function getVille(): string { return $this->ville; }
+    public function getEmail(): string { return $this->email; }
+    public function getSpecialite(): string { return $this->specialite; }
 }
