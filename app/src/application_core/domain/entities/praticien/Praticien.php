ac2a7f3f931d7e9c3f7874bb3fb841bd87b6ea9c
Hanan <hanan.ashrafi@lycee-faure.fr>
2025-09-23 15:07:20 +0200

    création prat repository servicespratinterfaces

--- app/src/application_core/domain/entities/praticien/Praticien.php
+++ app/src/application_core/domain/entities/praticien/Praticien.php
@@ -5,13 +5,14 @@ namespace toubilib\core\domain\entities\praticien;
 //entité practicien avec ces informations//
 class Praticien
 {
-   private string $nom;
-   private string $prenom;
-   private string $ville;
-   private string $email;
-   private string $specialite;
+    private string $nom;
+    private string $prenom;
+    private string $ville;
+    private string $email;
+    private string $specialite;
 
-    public function __construct(string $nom, string $prenom, string $ville, string $email, string $specialite) {
+    public function __construct(string $nom, string $prenom, string $ville, string $email, string $specialite)
+    {
         $this->nom = $nom;
         $this->prenom = $prenom;
         $this->ville = $ville;
@@ -20,9 +21,24 @@ class Praticien
     }
 
     //méthode d'accès aux attributs(get)//
-    public function getNom(): string { return $this->nom; }
-    public function getPrenom(): string { return $this->prenom; }
-    public function getVille(): string { return $this->ville; }
-    public function getEmail(): string { return $this->email; }
-    public function getSpecialite(): string { return $this->specialite; }
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+    public function getVille(): string
+    {
+        return $this->ville;
+    }
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+    public function getSpecialite(): string
+    {
+        return $this->specialite;
+    }
 }
